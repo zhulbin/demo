eb40cc3b3a557b749ef3011a88e7a364da4f49e3
xiaozhu <xiaozhuzlb@foxmail.com>
2022-07-22 11:55:20 +0800

    为主页增加redux

--- src/pages/main/home/index.tsx
+++ src/pages/main/home/index.tsx
@@ -1,10 +1,38 @@
 import React from "react";
+import { connect } from "react-redux";
+import { View, Button, Input } from "@tarojs/components";
+import { doGetValue } from "src/common"
+import State, * as Interface from 'src/interface'
 
-const Home: React.FC = () => {
+namespace Home {
+  export interface Props {
+    value: PickValue<Interface.Common.State, 'value'>
+  }
+}
+
+const Home: React.FC<Home.Props> = props => {
+
+  const [currentValue, setCurrentValue] = React.useState('')
+
+  const getVlue = e => {
+    setCurrentValue(e.target.value)
+  }
+
+  const handleValue = () => {
+    doGetValue(currentValue)
+  }
+
   return (
     <>
+      <View>数值器的值为：{props.value}</View>
+      <Input onInput={getVlue} />
+      <Button type="primary" onClick={() => handleValue()} >确定</Button>
     </>
   )
 }
 
-export default Home
+export default connect((state: State) => {
+  return({
+    value: state.common.value
+  })
+})(Home)
