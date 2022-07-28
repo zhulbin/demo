460f3e46ab2eee73a9582c295ee0e99d06453165
xiaozhu <xiaozhuzlb@foxmail.com>
2022-07-28 17:38:18 +0800

    修改config文件

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,28 @@
 export default {
   pages: [
-    'pages/main/home/index'
+    'pages/main/home/index',
+    'pages/main/order/index'
   ],
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black'
+  },
+  tabBar: {
+    custom: true,
+    list: [
+      {
+        pagePath: 'pages/main/home/index',
+        text: '主页'
+      },
+      {
+        pagePath: 'pages/main/order/index',
+        text: '点单'
+      }
+    ]
+  },
+  usingComponents: {
+
   }
 }
