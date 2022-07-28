e5a4c845966f59e7db58402837c43e40b0048c22
xiaozhu <xiaozhuzlb@foxmail.com>
2022-07-28 17:40:56 +0800

    修改并添加公共方法

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,6 +1,10 @@
-import { getValue } from "src/reducer/common";
+import Taro from '@tarojs/taro'
+import { getValue } from "src/reducer/test";
 import { store } from 'src/store'
 
 export const doGetValue = (value: string) => {
   return store.dispatch(getValue(value))
 }
+
+export const getActualSize = (size: number) => Math.floor(Taro.getSystemInfoSync().windowWidth * size / 750)
+
