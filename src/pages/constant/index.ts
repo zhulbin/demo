400941c34a2f718e8cab7f5bbcf76739a2d49445
xiaozhu <xiaozhuzlb@foxmail.com>
2022-07-28 17:35:26 +0800

    新增点单页中的menu的mock数据

---
+++ src/pages/constant/index.ts
@@ -0,0 +1,5 @@
+import * as OrderList from './orderList'
+
+export {
+  OrderList
+}
---
+++ src/pages/constant/orderList.ts
@@ -0,0 +1,175 @@
+export const list: Array<ObjectType> = [
+  {
+    name: 'A',
+    id: '001',
+    childId: '100',
+    title: 'A水果',
+    menuList: [
+      {text: 'A-苹果'},
+      {text: 'A-橘子'},
+      {text: 'A-芒果'},
+      {text: 'A-菠萝'},
+    ]
+  },
+  {
+    name: 'B',
+    id: '002',
+    childId: '200',
+    title: 'B农作物',
+    menuList: [
+      {text: 'B-水稻'},
+      {text: 'B-小麦'},
+    ]
+  },
+  {
+    name: 'C',
+    id: '003',
+    childId: '300',
+    title: 'C蔬菜',
+    menuList: [
+      {text: 'C-西红柿'},
+      {text: 'C-青菜'},
+      {text: 'C-玉米'},
+      {text: 'C-秋葵'},
+      {text: 'C-红薯'},
+      {text: 'C-土豆'},
+    ]
+  },
+  {
+    name: 'D',
+    id: '004',
+    childId: '400',
+    title: 'D家禽',
+    menuList: [
+      {text: 'D-大鹅'},
+      {text: 'D-公鸡'},
+      {text: 'D-鸭子'},
+    ]
+  },
+  {
+    name: 'E',
+    id: '005',
+    childId: '500',
+    title: 'E肉制品',
+    menuList: [
+      {text: 'E-鱼肉'},
+      {text: 'E-鸡肉'},
+      {text: 'E-猪肉'},
+      {text: 'E-牛肉'},
+      {text: 'E-羊肉'},
+    ]
+  },
+  {
+    name: 'F',
+    id: '006',
+    childId: '600',
+    title: 'F动物园',
+    menuList: [
+      {text: 'F-狮子'},
+      {text: 'F-长颈鹿'},
+      {text: 'F-黑天鹅'},
+      {text: 'F-老虎'},
+      {text: 'F-鳄鱼'},
+    ]
+  },
+  {
+    name: 'G',
+    id: '007',
+    childId: '700',
+    title: 'G宠物',
+    menuList: [
+      {text: 'G-狗狗'},
+      {text: 'G-小猫'},
+      {text: 'G-蛇'},
+      {text: 'G-老鼠'},
+    ]
+  },
+  {
+    name: 'H',
+    id: '008',
+    childId: '800',
+    title: 'H交通工具',
+    menuList: [
+      {text: 'H-汽车'},
+      {text: 'H-飞机'},
+      {text: 'H-高铁'},
+      {text: 'H-自行车'},
+    ]
+  },
+  {
+    name: 'I',
+    id: '009',
+    childId: '900',
+    title: 'I海洋鱼类',
+    menuList: [
+      {text: 'I-鲨鱼'},
+      {text: 'I-小丑鱼'},
+      {text: 'I-鲸鱼'},
+      {text: 'I-飞鱼'}
+    ]
+  },
+  {
+    name: 'J',
+    id: '0010',
+    childId: '1000',
+    title: 'J运动品牌',
+    menuList: [
+      {text: 'J-nike'},
+      {text: 'J-adidas'},
+      {text: 'J-anta'},
+      {text: 'J-lining'}
+    ]
+  },
+  {
+    name: 'K',
+    id: '0011',
+    childId: '1100',
+    title: 'K手机品牌',
+    menuList: [
+      {text: 'K-苹果'},
+      {text: 'K-华为'},
+      {text: 'K-小米'},
+      {text: 'K-三星'},
+      {text: 'K-OPPO'},
+      {text: 'K-vivo'},
+    ]
+  },
+  {
+    name: 'L',
+    id: '0012',
+    childId: '1200',
+    title: 'L运动',
+    menuList: [
+      {text: 'L-跑步'},
+      {text: 'L-蹦极'},
+      {text: 'L-跳伞'},
+      {text: 'L-跳绳'},
+      {text: 'L-潜水'},
+    ]
+  },
+  {
+    name: 'M',
+    id: '0013',
+    childId: '1300',
+    title: 'M国家',
+    menuList: [
+      {text: 'M-中国'},
+      {text: 'M-同盟国'},
+      {text: 'M-岛国'},
+      {text: 'M-漂亮国'},
+      {text: 'M-战败国'},
+    ]
+  },
+  {
+    name: 'N',
+    id: '0014',
+    childId: '1400',
+    title: 'N城市',
+    menuList: [
+      {text: 'N-北京'},
+      {text: 'N-上海'},
+      {text: 'N-广州'},
+      {text: 'N-深圳'},
+    ]
+  }
+]
